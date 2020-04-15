0680cd893ae0088050f592915bb5a697fffb2208
Gaku Sasaki <gaku.sasaki@gmail.com>
2020-04-14 20:09:41 -0700

    half of model and some of controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using dojodachi.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace dojodachi.Controllers
 {
@@ -12,7 +14,37 @@ namespace dojodachi.Controllers
     {
         public IActionResult Index()
         {
+            // Very first render: make Dojodachi obj
+            if (HttpContext.Session.GetObjectFromJson<DojodachiModel>("DD") == null)
+            {
+                DojodachiModel dojodachi = new DojodachiModel();
+                HttpContext.Session.SetObjectAsJson("DD",dojodachi);
+            }
+            ViewBag.DD = HttpContext.Session.GetObjectFromJson<DojodachiModel>("DD");
+
+            // Very first render: start with a different line
+            if (HttpContext.Session.GetString("Result") == null)
+            {
+                HttpContext.Session.SetString("Result","Welcome! Let's get started.");
+            } 
+            ViewBag.Result = HttpContext.Session.GetString("Result");
+            
+            // "Mode" for the bottom partials
+            if (HttpContext.Session.GetString("Partial") == null)
+            {
+                HttpContext.Session.SetString("Partial","Regular");
+            } 
+            ViewBag.Partial = HttpContext.Session.GetString("Partial");
+
             return View();
+        }
+
+        [HttpGet("/feed")]
+        public IActionResult Feed()
+        {
+            // run feed logic on Dojodachi obj
+            DojodachiModel dojodachi = new DojodachiModel();
+            return RedirectToAction("Index", dojodachi);
         }
 
         public IActionResult Privacy()
@@ -24,6 +56,20 @@ namespace dojodachi.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
     }
 }
